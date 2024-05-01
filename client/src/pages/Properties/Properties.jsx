560540eb1788693bd44e9f1f01ef566ea6676991
Adam Mulani <aadammulani12@gmail.com>
2024-05-01 19:10:26 +0530

    designing of search bar is done on Properties page

--- client/src/pages/Properties/Properties.jsx
+++ client/src/pages/Properties/Properties.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { SearchBar } from '../../components/SearchBar/SearchBar'
+import "./Properties.css"
 export const Properties = () => {
   return (
-    <div className='wrapper'>Properties</div>
+    <div className='wrapper'>
+        <div className="flexColCenter paddings innerWidth properties-container">
+            <SearchBar/>
+        </div>
+    </div>
   )
 }
