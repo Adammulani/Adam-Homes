1d0acf3694cae0121393f208251199db188fe382
Adam Mulani <aadammulani12@gmail.com>
2024-05-04 12:26:01 +0530

    Implemented Authentication using Auth0

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,8 +3,12 @@ import './Header.css'
 import {BiMenuAltRight} from 'react-icons/bi';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { Link, NavLink } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import ProfileMenu from '../ProfileMenu/ProfileMenu.jsx';
+import { MantineProvider } from '@mantine/core';
 const Header = () => {
   const [menuOpened,setMenuOpened]=useState(false);
+  const {loginWithRedirect,isAuthenticated,user,logout}=useAuth0()
 
   const getMenuStyles=(menuOpened)=>{
     if(document.documentElement.clientWidth <=800){
@@ -14,9 +18,8 @@ const Header = () => {
   return (
     <section className="h-wrapper">
       <div className="flexCenter paddings innerWidth h-container">
-       
         <Link to="/">
-        <img src="./logo.png" alt="logo" width={100}></img>
+          <img src="./logo.png" alt="logo" width={100}></img>
         </Link>
 
         <OutsideClickHandler
@@ -25,15 +28,30 @@ const Header = () => {
           }}
         >
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-           
-           <NavLink to="/properties">Properties</NavLink>
-              <a href="mailto:aadammulani12@gmail.com">Contact</a>
+            <NavLink to="/properties">Properties</NavLink>
+            <a href="mailto:aadammulani12@gmail.com">Contact</a>
 
-              {/* login button */}
-              <button className="button">
+            {/* login button */}
+            {
+              !isAuthenticated?
+              (<button className="button" onClick={loginWithRedirect}>
                 Login
-              </button>
-           
+              </button> ):
+              (
+                /*<MantineProvider>
+                    <ProfileMenu user={user} logout={logout}/>
+                </MantineProvider>
+             */
+               <ProfileMenu user={user} logout={logout}/>
+                //<div>User Profile</div>
+               /* <button className="button" onClick={()=>{
+                  localStorage.clear();
+                  logout();
+                }}>
+                  Logout
+                </button>*/
+              )
+            }
           </div>
         </OutsideClickHandler>
 
